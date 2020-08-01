6548e9750cb54538418017d517d127bc5600bafb
jmarcelocruz <zemarcelocruz@gmail.com>
2020-08-01 14:42:03 +0100

    Open and close UDP socket; Can send commands (in development)

--- API/ANIDB/ANIDB_UDP.py
+++ API/ANIDB/ANIDB_UDP.py
@@ -2,16 +2,27 @@ import json
 import socket
 import os
 
-SERVER_ADDRESS_FILE = 'ANIDB_API_SERVER.json'
+SERVER_ADDRESS_FILE = 'API/ANIDB/ANIDB_API_SERVER.json'
 class UDP_Client:
 	def __init__(self):
 		if os.path.exists(SERVER_ADDRESS_FILE):
 			with open(SERVER_ADDRESS_FILE, 'r') as json_file:
 				data = json.load(json_file)
-				self.server_ip = socket.gethostbyname(data['ANIDB_HOSTNAME'])
-				self.server_port = data['ANIDB_PORT']
+				addrinfo = socket.getaddrinfo(data['ANIDB_HOSTNAME'], data['ANIDB_PORT'], proto=socket.IPPROTO_UDP)
+				self.family = addrinfo[0][0]
+				self.address = addrinfo[0][4] 
+				json_file.close()
 		else:
 			print('ERROR: ' + SERVER_ADDRESS_FILE + ' not found!')
+			exit()
 
-	def connect_server(self):
-		
+	def open_socket(self):
+		self.socket = socket.socket(self.family, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+	def close_socket(self):
+		self.socket.close()
+
+	def communicate(self, message):
+		self.socket.sendto(message.encode("ascii"), self.address)
+		data = self.socket.recv(1024).decode("ascii")
+		print(data)
