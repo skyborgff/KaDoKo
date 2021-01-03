ba50dfdf94c93adffc35618917ec0b8d977b970f
Fábio Ferreira <fabiorcferreira@gmail.com>
2021-01-03 11:20:35 +0000

    Merged UI and Main TaskManager

--- Core/API/FlaskAPI.py
+++ Core/API/FlaskAPI.py
@@ -3,7 +3,8 @@ from flask_cors import CORS, cross_origin
 import os
 import multiprocessing
 from multiprocessing import Process, Manager
-from Core.API.TaskManager import Tasker
+# from Core.API.TaskManager import Tasker
+from Core.Tasker.TaskManager import Tasker, TaskImportance, TaskType
 
 
 class FlaskAPI:
@@ -30,13 +31,13 @@ class FlaskAPI:
         @self.app.route('/library_list', methods=['GET'])
         def library_list():
             # info = self.kadoko.ui.library_list()
-            info = self.tasker.addWaitReply('library_list')
+            info = self.tasker.addWaitReply('library_list', TaskImportance.UI, TaskType.SYNC)
             return jsonify(info)
 
         @self.app.route('/db_list', methods=['GET'])
         def db_list():
             # info = self.kadoko.ui.db_list()
-            info = self.tasker.addWaitReply('db_list')
+            info = self.tasker.addWaitReply('db_list', TaskImportance.UI, TaskType.SYNC)
             return jsonify(info)
 
         @self.app.route('/setup_settings', methods=['POST'])
@@ -44,14 +45,14 @@ class FlaskAPI:
             response_object = {'status': 'success'}
             post_data = request.get_json()
             # self.kadoko.ui.setup_settings(post_data)
-            self.tasker.addTask('setup_settings', [post_data])
+            self.tasker.addTask('setup_settings', TaskImportance.UI, TaskType.SYNC, [post_data])
             response_object['message'] = 'Settings Received'
             return jsonify(response_object)
 
         @self.app.route('/get_authentication_needed', methods=['GET'])
         def get_authentication_needed():
             # info = self.kadoko.ui.get_authentication_needed()
-            info = self.tasker.addWaitReply('get_authentication_needed')
+            info = self.tasker.addWaitReply('get_authentication_needed', TaskImportance.UI, TaskType.SYNC)
             return jsonify(info)
 
         @self.app.route('/set_authentication', methods=['POST'])
@@ -59,7 +60,7 @@ class FlaskAPI:
             response_object = {'status': 'success'}
             post_data = request.get_json()
             # self.kadoko.ui.set_authentication(post_data)
-            self.tasker.addTask('set_authentication', [post_data])
+            self.tasker.addTask('set_authentication', TaskImportance.UI, TaskType.SYNC, [post_data])
             response_object['message'] = 'Auth Received'
             return jsonify(response_object)
 
@@ -70,7 +71,7 @@ class FlaskAPI:
             # print('Flask')
             # print(self.taskDict)
             # print('Sending Graph')
-            self.tasker.addWaitReply('generate_graph', ["Core/API/FlaskFiles/force/force.json"])
+            self.tasker.addWaitReply('generate_graph', TaskImportance.UI, TaskType.SYNC, ["Core/API/FlaskFiles/force/force.json"])
             return send_from_directory('FlaskFiles/force', path)
 
         self.app.run('localhost', 8283, False)
