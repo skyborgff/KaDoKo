34d5fdd77933c23c52fdd27335f57e1c680aa581
Fábio Ferreira <fabiorcferreira@gmail.com>
2021-02-15 23:38:36 +0000

    Fixing responsive navbar

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -3,15 +3,6 @@
       <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">
               <img src="../assets/logoCrop.png" height="30" class="d-inline-block align-top" alt="">
           KaDoKo</a>
-
-      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
     </nav>
 
 </template>
